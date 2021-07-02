f480e9d0dd5b17bc14594041b530d35c5dee2062
Alexey Gribov <m0chk0z@gmail.com>
2021-07-02 17:43:48 +0300

    Implement AddCategory command, related parameter and unit tests

---
+++ ToDoAppDataAccessLayer/Commands/AddCategoryCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using Dapper;
+using ToDoAppDomainLayer.Interfaces;
+using ToDoAppDomainLayer.Parameters.Commands;
+
+namespace ToDoAppDataAccessLayer.Commands
+{
+    public class AddCategoryCommand : ICommand<AddCategoryCommandParameter, int>
+    {
+        private NpgsqlConnection Connection { get; set; }
+        public AddCategoryCommand(NpgsqlConnection connection)
+        {
+            if (connection is null)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
+            this.Connection = connection;
+        }
+
+        public int Execute(AddCategoryCommandParameter param)
+        {
+            string insertQuery = "INSERT INTO category (title, color) VALUES (@Title, @Color)";
+            string selectQuery = "SELECT (id) FROM category WHERE title = @Title AND color = @Color";
+
+            var affected = Connection.Execute(insertQuery, param.CategoryToAdd);
+            if (affected == 0) throw new Exception("Insert failed");
+            int insertedId = (int)Connection.ExecuteScalar(selectQuery, param.CategoryToAdd);
+            return insertedId;
+        }
+    }
+}
---
+++ ToDoAppUnitTests/Commands/AddCategoryCommandUnitTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Npgsql;
+using Dapper;
+using ToDoAppDomainLayer.DataObjects;
+using ToDoAppDataAccessLayer.Commands;
+using ToDoAppDomainLayer.Parameters.Commands;
+
+namespace ToDoAppUnitTests.Commands
+{
+    [Collection("Sequential")]
+    public class AddCategoryCommandUnitTests : TestBase
+    {
+        [Fact]
+        public void Test_InsertsOnValidInput()
+        {
+            // Arrange
+            var title = "Category 5";
+            var color = "#abcdef";
+            using var connection = new NpgsqlConnection(ConnectionString);
+            AddCategoryCommand sut = new AddCategoryCommand(connection);
+            // Act
+            sut.Execute(new AddCategoryCommandParameter
+            {
+                CategoryToAdd = new Category
+                {
+                    Title = title,
+                    Color = color,
+                }
+            });
+            // Assert
+            var count = connection.ExecuteScalar<int>("SELECT COUNT(*) FROM category");
+            Assert.Equal(5, count);
+            var inserted = connection.Query<Category>("SELECT (id, title, color) FROM category " +
+                "WHERE title = @Title AND color = @Color",
+                new
+                {
+                    Title = title,
+                    Color = color
+                })
+                .FirstOrDefault();
+            Assert.NotNull(inserted);
+        }
+
+        [Fact]
+        public void Test_ThrowsOnTryingToInsertAlreadyExistingValue()
+        {
+            // Arrange
+            var title = "Category 1";
+            var color = "#3f3f2f";
+            using var connection = new NpgsqlConnection(ConnectionString);
+            AddCategoryCommand sut = new AddCategoryCommand(connection);
+            // Act
+            // Assert
+            Assert.ThrowsAny<Exception>(() =>
+            {
+                sut.Execute(new AddCategoryCommandParameter
+                {
+                    CategoryToAdd = new Category
+                    {
+                        Title = title,
+                        Color = color,
+                    }
+                });
+            });
+        }
+
+        [Fact]
+        public void Test_ThrowsOnInvalidColorString()
+        {
+            // Arrange
+            var title = "Category 1";
+            var color = "#3f3f2g";
+            using var connection = new NpgsqlConnection(ConnectionString);
+            AddCategoryCommand sut = new AddCategoryCommand(connection);
+            // Act
+            // Assert
+            Assert.ThrowsAny<Exception>(() =>
+            {
+                sut.Execute(new AddCategoryCommandParameter
+                {
+                    CategoryToAdd = new Category
+                    {
+                        Title = title,
+                        Color = color,
+                    }
+                });
+            });
+        }
+    }
+}
