b4f3ddcc0fd1d58e63b6e7119ba08729273a1769
Alexey Gribov <m0chk0z@gmail.com>
2021-07-02 17:45:51 +0300

    Implement EditTask command, related parameter and unit tests

---
+++ ToDoAppDataAccessLayer/Commands/EditTaskCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using Dapper;
+using ToDoAppDomainLayer.Interfaces;
+using ToDoAppDomainLayer.Parameters.Commands;
+using ToDoAppDomainLayer.DataObjects;
+
+namespace ToDoAppDataAccessLayer.Commands
+{
+    public class EditTaskCommand : ICommand<EditTaskCommandParameter, int>
+    {
+        private NpgsqlConnection Connection { get; set; }
+        public EditTaskCommand(NpgsqlConnection connection)
+        {
+            if (connection is null)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
+            this.Connection = connection;
+        }
+        public int Execute(EditTaskCommandParameter param)
+        {
+            string updateQuery =
+                "UPDATE todotask SET (title, description, createdat, isactive, iscomplete, categoryid) = " +
+                "(@Title, @Description, @CreatedAt, @IsActive, @IsComplete, @CategoryId) WHERE id = @Id";
+            var affected = Connection.Execute(updateQuery, param.Task);
+            if (affected == 0) throw new Exception("Update failed");
+            int updatedId = param.Task.Id;
+            return updatedId;
+        }
+    }
+}
---
+++ ToDoAppUnitTests/Commands/EditTaskCommandUnitTests.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Npgsql;
+using Dapper;
+using ToDoAppDomainLayer.DataObjects;
+using ToDoAppDataAccessLayer.Commands;
+using ToDoAppDomainLayer.Parameters.Commands;
+
+namespace ToDoAppUnitTests.Commands
+{
+    [Collection("Sequential")]
+    public class EditTaskCommandUnitTests : TestBase
+    {
+        [Fact]
+        public void Test_UpdatesOnValidInput()
+        {
+            // Arrange
+            var id = 1;
+            var title = "Updated task";
+            var description = "Updated description";
+            var createdAt = new DateTime(2000, 7, 2, 5, 0, 0);
+            var isActive = true;
+            var isComplete = true;
+            var categoryId = 1;
+            using var connection = new NpgsqlConnection(ConnectionString);
+            EditTaskCommand sut = new EditTaskCommand(connection);
+            // Act
+            sut.Execute(new EditTaskCommandParameter
+            {
+                Task = new ToDoTask
+                {
+                    Id = id,
+                    Title = title,
+                    Description = description,
+                    CreatedAt = createdAt,
+                    IsActive = isActive,
+                    IsComplete = isComplete,
+                    CategoryId = categoryId
+                }
+            });
+            // Assert
+            string selectQuery = 
+                "SELECT (id, title, description, createdat, " +
+                "isactive, iscomplete, categoryid) FROM todotask " +
+                "WHERE " +
+                "title = @Title AND " +
+                "description = @Description AND " +
+                "createdat = @CreatedAt AND " +
+                "isactive = @IsActive AND " +
+                "iscomplete = @IsComplete AND " +
+                "categoryid = @CategoryId"; ;
+            var updated = connection.Query<Category>(selectQuery,
+                new
+                {
+                    Id = id,
+                    Title = title,
+                    Description = description,
+                    CreatedAt = createdAt,
+                    IsActive = isActive,
+                    IsComplete = isComplete,
+                    CategoryId = categoryId,
+                })
+                .FirstOrDefault();
+            Assert.NotNull(updated);
+        }
+
+        [Fact]
+        public void Test_ThrowsOnInvalidId()
+        {
+            // Arrange
+            var id = 7; // invalid id
+            var title = "Updated task";
+            var description = "Updated description";
+            var createdAt = new DateTime(2000, 7, 2, 5, 0, 0);
+            var isActive = true;
+            var isComplete = true;
+            var categoryId = 1;
+            using var connection = new NpgsqlConnection(ConnectionString);
+            EditTaskCommand sut = new EditTaskCommand(connection);
+            // Act
+            Assert.ThrowsAny<Exception>(() =>
+            {
+                sut.Execute(new EditTaskCommandParameter
+                {
+                    Task = new ToDoTask
+                    {
+                        Id = id,
+                        Title = title,
+                        Description = description,
+                        CreatedAt = createdAt,
+                        IsActive = isActive,
+                        IsComplete = isComplete,
+                        CategoryId = categoryId
+                    }
+                });
+            });
+        }
+
+        [Fact]
+        public void Test_ThrowsOnForeignKeyConstraintViolation()
+        {
+            // Arrange
+            var id = 1; 
+            var title = "Updated task";
+            var description = "Updated description";
+            var createdAt = new DateTime(2000, 7, 2, 5, 0, 0);
+            var isActive = true;
+            var isComplete = true;
+            var categoryId = 5; // invalid id
+            using var connection = new NpgsqlConnection(ConnectionString);
+            EditTaskCommand sut = new EditTaskCommand(connection);
+            // Act
+            Assert.ThrowsAny<Exception>(() =>
+            {
+                sut.Execute(new EditTaskCommandParameter
+                {
+                    Task = new ToDoTask
+                    {
+                        Id = id,
+                        Title = title,
+                        Description = description,
+                        CreatedAt = createdAt,
+                        IsActive = isActive,
+                        IsComplete = isComplete,
+                        CategoryId = categoryId
+                    }
+                });
+            });
+        }
+    }
+}
