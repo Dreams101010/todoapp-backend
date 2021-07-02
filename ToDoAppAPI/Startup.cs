cdff6c14e0d7f79a986ef03e5f21b317efd9b5fc
Alexey Gribov <m0chk0z@gmail.com>
2021-07-02 17:38:03 +0300

    Add NpgsqlConnection to DI container

--- ToDoAppAPI/Startup.cs
+++ ToDoAppAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Autofac.Extensions.DependencyInjection;
 using Autofac;
+using Npgsql;
 
 namespace ToDoAppAPI
 {
@@ -32,6 +33,8 @@ namespace ToDoAppAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient(
+                (x) => new NpgsqlConnection(Configuration.GetConnectionString("PostgresDB")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
