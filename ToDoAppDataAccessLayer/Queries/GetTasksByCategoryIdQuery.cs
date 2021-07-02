fc4fec60fff9b3757e0ace8abc27e11d6d950c58
Alexey Gribov <m0chk0z@gmail.com>
2021-07-02 17:47:00 +0300

    Implement GetTasksByCategoryId query, related parameter and unit tests

---
+++ ToDoAppDataAccessLayer/Queries/GetTasksByCategoryIdQuery.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using Dapper;
+using ToDoAppDomainLayer.Interfaces;
+using ToDoAppDomainLayer.Parameters.Queries;
+using ToDoAppDomainLayer.DataObjects;
+
+namespace ToDoAppDataAccessLayer.Queries
+{
+    public class GetTasksByCategoryIdQuery 
+        : IQuery<GetTasksByCategoryIdQueryParameter, IEnumerable<ToDoTaskOutputModel>>
+    {
+        private NpgsqlConnection Connection { get; set; }
+
+        public GetTasksByCategoryIdQuery(NpgsqlConnection connection)
+        {
+            if (connection is null)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
+            this.Connection = connection;
+        }
+
+        public IEnumerable<ToDoTaskOutputModel> Execute(
+            GetTasksByCategoryIdQueryParameter param)
+        {
+            string selectQuery = "SELECT todotask.id, todotask.title, todotask.description, todotask.createdat, " +
+                "todotask.isactive, todotask.iscomplete, todotask.categoryId, category.title AS categoryTitle, " +
+                "category.color AS categoryColor FROM todotask JOIN category ON todotask.categoryId = category.id " +
+                "WHERE categoryId = @CategoryId;";
+            return Connection.Query<ToDoTaskOutputModel>(selectQuery, param).ToList();
+        }
+    }
+}
---
+++ ToDoAppUnitTests/Queries/GetTasksByCategoryIdQueryUnitTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Npgsql;
+using Dapper;
+using ToDoAppDomainLayer.DataObjects;
+using ToDoAppDataAccessLayer.Queries;
+using ToDoAppDomainLayer.Parameters.Queries;
+
+namespace ToDoAppUnitTests.Queries
+{
+    [Collection("Sequential")]
+    public class GetTasksByCategoryIdQueryUnitTests : TestBase
+    {
+        [Fact]
+        public void Test_ReturnsCorrectCollectionSize()
+        {
+            // Arrange
+            using var connection = new NpgsqlConnection(ConnectionString);
+            GetTasksByCategoryIdQuery sut = new GetTasksByCategoryIdQuery(connection);
+            // Act
+            var result = sut.Execute(new GetTasksByCategoryIdQueryParameter
+            {
+                CategoryId = 1,
+            });
+            // Assert
+            Assert.Equal(3, result.Count());
+        }
+    }
+}
