6d43dc0c1928bdd815cedb6e8da001ab39e8535f
Alexey Gribov <m0chk0z@gmail.com>
2021-07-02 17:44:41 +0300

    Implement RemoveCategory command, related parameter and unit tests

---
+++ ToDoAppDataAccessLayer/Commands/RemoveCategoryCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using Dapper;
+using ToDoAppDomainLayer.Interfaces;
+using ToDoAppDomainLayer.Parameters.Commands;
+
+namespace ToDoAppDataAccessLayer.Commands
+{
+    public class RemoveCategoryCommand : ICommand<RemoveCategoryCommandParameter, int>
+    {
+        private NpgsqlConnection Connection { get; set; }
+
+        public RemoveCategoryCommand(NpgsqlConnection connection)
+        {
+            if (connection is null)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
+            this.Connection = connection;
+        }
+
+        public int Execute(RemoveCategoryCommandParameter param)
+        {
+            string removeQuery = "DELETE FROM category WHERE id = @CategoryId";
+
+            var affected = Connection.Execute(removeQuery, param);
+            if (affected == 0) throw new Exception("Remove failed");
+            int removedId = param.CategoryId;
+            return removedId;
+        }
+    }
+}
---
+++ ToDoAppUnitTests/Commands/RemoveCategoryCommandUnitTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Npgsql;
+using Dapper;
+using ToDoAppDomainLayer.DataObjects;
+using ToDoAppDataAccessLayer.Commands;
+using ToDoAppDomainLayer.Parameters.Commands;
+
+namespace ToDoAppUnitTests.Commands
+{
+    [Collection("Sequential")]
+    public class RemoveCategoryCommandUnitTests : TestBase
+    {
+        [Fact]
+        public void Test_RemovesOnValidInput()
+        {
+            // Arrange
+            int removedId = 3;
+            using var connection = new NpgsqlConnection(ConnectionString);
+            RemoveCategoryCommand sut = new RemoveCategoryCommand(connection);
+            // Act
+            sut.Execute(new RemoveCategoryCommandParameter
+            {
+               CategoryId = removedId,
+            });
+            // Assert
+            var count = connection.ExecuteScalar<int>("SELECT COUNT(*) FROM category");
+            Assert.Equal(3, count);
+            var removed = connection.Query<Category>("SELECT (id, title, color) FROM category " +
+                "WHERE id = @Id",
+                new
+                {
+                    Id = removedId,
+                })
+                .FirstOrDefault();
+            Assert.Null(removed);
+        }
+
+        [Fact]
+        public void Test_ThrowsOnNonExistingId()
+        {
+            int removedId = 6;
+            using var connection = new NpgsqlConnection(ConnectionString);
+            RemoveCategoryCommand sut = new RemoveCategoryCommand(connection);
+            // Act
+            // Assert
+            Assert.ThrowsAny<Exception>(() =>
+            {
+                sut.Execute(new RemoveCategoryCommandParameter
+                {
+                    CategoryId = removedId,
+                });
+            });
+        }
+
+        [Fact]
+        public void Test_ThrowsOnForeignKeyConstraintViolation()
+        {
+            int removedId = 1;
+            using var connection = new NpgsqlConnection(ConnectionString);
+            RemoveCategoryCommand sut = new RemoveCategoryCommand(connection);
+            // Act
+            // Assert
+            Assert.ThrowsAny<Exception>(() =>
+            {
+                sut.Execute(new RemoveCategoryCommandParameter
+                {
+                    CategoryId = removedId,
+                });
+            });
+        }
+    }
+}
