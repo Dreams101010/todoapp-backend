946af54529ea8f5356c57234b16449374203a5d5
Alexey Gribov <m0chk0z@gmail.com>
2021-07-02 18:28:35 +0300

    Implement Task facade class

---
+++ ToDoAppDomainLayer/Facades/TaskFacade.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoAppDomainLayer.Interfaces.Facades;
+using ToDoAppDomainLayer.Interfaces;
+using ToDoAppDomainLayer.Parameters.Commands;
+using ToDoAppDomainLayer.Parameters.Queries;
+using ToDoAppDomainLayer.DataObjects;
+
+
+namespace ToDoAppDomainLayer.Facades
+{
+    using AddTaskCommand = ICommand<AddTaskCommandParameter, int>;
+    using EditTaskCommand = ICommand<EditTaskCommandParameter, int>;
+    using RemoveTaskCommand = ICommand<RemoveTaskCommandParameter, int>;
+    using GetTasksByCategoryIdQuery = IQuery<GetTasksByCategoryIdQueryParameter,
+            IEnumerable<ToDoTaskOutputModel>>;
+    using GetActiveTasksQuery = IQuery<GetActiveTasksQueryParameter,
+            IEnumerable<ToDoTaskOutputModel>>;
+    using GetActiveTasksByCategoryIdQuery = IQuery<GetActiveTasksByCategoryIdQueryParameter,
+            IEnumerable<ToDoTaskOutputModel>>;
+    using GetCompleteTasksQuery = IQuery<GetCompleteTasksQueryParameter,
+            IEnumerable<ToDoTaskOutputModel>>;
+    using GetCompleteTasksByCategoryIdQuery = IQuery<GetCompleteTasksByCategoryIdQueryParameter,
+            IEnumerable<ToDoTaskOutputModel>>;
+    public class TaskFacade : ITaskFacade
+    {
+        private readonly AddTaskCommand AddTaskCommand = null;
+        private readonly EditTaskCommand EditTaskCommand = null;
+        private readonly RemoveTaskCommand RemoveTaskCommand = null;
+        private readonly GetTasksByCategoryIdQuery GetTasksByCategoryIdQuery = null;
+        private readonly GetActiveTasksQuery GetActiveTasksQuery = null;
+        private readonly GetActiveTasksByCategoryIdQuery GetActiveTasksByCategoryIdQuery = null;
+        private readonly GetCompleteTasksQuery GetCompleteTasksQuery = null;
+        private readonly GetCompleteTasksByCategoryIdQuery GetCompleteTasksByCategoryIdQuery = null;
+
+        public TaskFacade(
+            AddTaskCommand addTaskCommand,
+            EditTaskCommand editTaskCommand,
+            RemoveTaskCommand removeTaskCommand,
+            GetTasksByCategoryIdQuery getTasksByCategoryIdQuery,
+            GetActiveTasksQuery getActiveTasksQuery,
+            GetActiveTasksByCategoryIdQuery getActiveTasksByCategoryIdQuery,
+            GetCompleteTasksQuery getCompleteTasksQuery,
+            GetCompleteTasksByCategoryIdQuery GetCompleteTasksByCategoryIdQuery)
+        {
+            if (addTaskCommand is null)
+            {
+                throw new ArgumentNullException(nameof(addTaskCommand));
+            }
+
+            if (editTaskCommand is null)
+            {
+                throw new ArgumentNullException(nameof(editTaskCommand));
+            }
+
+            if (removeTaskCommand is null)
+            {
+                throw new ArgumentNullException(nameof(removeTaskCommand));
+            }
+
+            if (getTasksByCategoryIdQuery is null)
+            {
+                throw new ArgumentNullException(nameof(getTasksByCategoryIdQuery));
+            }
+
+            if (getActiveTasksQuery is null)
+            {
+                throw new ArgumentNullException(nameof(getActiveTasksQuery));
+            }
+
+            if (getActiveTasksByCategoryIdQuery is null)
+            {
+                throw new ArgumentNullException(nameof(getActiveTasksByCategoryIdQuery));
+            }
+
+            if (getCompleteTasksQuery is null)
+            {
+                throw new ArgumentNullException(nameof(getCompleteTasksQuery));
+            }
+
+            if (GetCompleteTasksByCategoryIdQuery is null)
+            {
+                throw new ArgumentNullException(nameof(GetCompleteTasksByCategoryIdQuery));
+            }
+            this.AddTaskCommand = addTaskCommand;
+            this.EditTaskCommand = editTaskCommand;
+            this.RemoveTaskCommand = removeTaskCommand;
+            this.GetTasksByCategoryIdQuery = getTasksByCategoryIdQuery;
+            this.GetActiveTasksQuery = getActiveTasksQuery;
+            this.GetActiveTasksByCategoryIdQuery = getActiveTasksByCategoryIdQuery;
+            this.GetCompleteTasksQuery = getCompleteTasksQuery;
+            this.GetCompleteTasksByCategoryIdQuery = GetCompleteTasksByCategoryIdQuery;
+        }
+
+        public int AddTask(ToDoTask taskToAdd)
+        {
+            if (taskToAdd is null)
+            {
+                throw new ArgumentNullException(nameof(taskToAdd));
+            }
+
+            AddTaskCommandParameter param = new()
+            {
+                TaskToAdd = taskToAdd,
+            };
+            return AddTaskCommand.Execute(param);
+        }
+        public void EditTask(ToDoTask taskToEdit)
+        {
+            if (taskToEdit is null)
+            {
+                throw new ArgumentNullException(nameof(taskToEdit));
+            }
+
+            EditTaskCommandParameter param = new()
+            {
+                Task = taskToEdit,
+            };
+            EditTaskCommand.Execute(param);
+        }
+
+        public void RemoveTaskById(int taskId)
+        {
+            RemoveTaskCommandParameter param = new()
+            {
+                TaskId = taskId,
+            };
+            RemoveTaskCommand.Execute(param);
+        }
+
+        public IEnumerable<ToDoTaskOutputModel> GetTasksByCategoryId(int categoryId)
+        {
+            GetTasksByCategoryIdQueryParameter param = new()
+            {
+                CategoryId = categoryId,
+            };
+            return GetTasksByCategoryIdQuery.Execute(param);
+        }
+
+        public IEnumerable<ToDoTaskOutputModel> GetActiveTasks()
+        {
+            GetActiveTasksQueryParameter param = new();
+            return GetActiveTasksQuery.Execute(param);
+        }
+        public IEnumerable<ToDoTaskOutputModel> GetActiveTasksByCategoryId(int categoryId)
+        {
+            GetActiveTasksByCategoryIdQueryParameter param = new()
+            {
+                CategoryId = categoryId,
+            };
+            return GetActiveTasksByCategoryIdQuery.Execute(param);
+        }
+        public IEnumerable<ToDoTaskOutputModel> GetCompletedTasks()
+        {
+            GetCompleteTasksQueryParameter param = new();
+            return GetCompleteTasksQuery.Execute(param);
+        }
+        public IEnumerable<ToDoTaskOutputModel> GetCompletedTasksByCategoryId(int categoryId)
+        {
+            GetCompleteTasksByCategoryIdQueryParameter param = new()
+            {
+                CategoryId = categoryId,
+            };
+            return GetCompleteTasksByCategoryIdQuery.Execute(param);
+        }
+    }
+}
---
+++ ToDoAppDomainLayer/Interfaces/Facades/ITaskFacade.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoAppDomainLayer.DataObjects;
+
+namespace ToDoAppDomainLayer.Interfaces.Facades
+{
+    public interface ITaskFacade
+    {
+        public int AddTask(ToDoTask taskToAdd);
+        public void EditTask(ToDoTask taskToEdit);
+        public void RemoveTaskById(int taskId);
+        public IEnumerable<ToDoTaskOutputModel> GetTasksByCategoryId(int categoryId);
+        public IEnumerable<ToDoTaskOutputModel> GetActiveTasks();
+        public IEnumerable<ToDoTaskOutputModel> GetActiveTasksByCategoryId(int categoryId);
+        public IEnumerable<ToDoTaskOutputModel> GetCompletedTasks();
+        public IEnumerable<ToDoTaskOutputModel> GetCompletedTasksByCategoryId(int categoryId);
+    }
+}
