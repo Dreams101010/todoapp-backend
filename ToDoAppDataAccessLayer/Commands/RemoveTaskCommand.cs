001731a3e4e9955d4732856ee7e049694c074c37
Alexey Gribov <m0chk0z@gmail.com>
2021-07-02 17:46:07 +0300

    Implement RemoveTask command, related parameter and unit tests

---
+++ ToDoAppDataAccessLayer/Commands/RemoveTaskCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using Dapper;
+using ToDoAppDomainLayer.Interfaces;
+using ToDoAppDomainLayer.Parameters.Commands;
+
+
+namespace ToDoAppDataAccessLayer.Commands
+{
+    public class RemoveTaskCommand : ICommand<RemoveTaskCommandParameter, int>
+    {
+        private NpgsqlConnection Connection { get; set; }
+
+        public RemoveTaskCommand(NpgsqlConnection connection)
+        {
+            if (connection is null)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
+            this.Connection = connection;
+        }
+
+        public int Execute(RemoveTaskCommandParameter param)
+        {
+            string removeQuery = "DELETE FROM todotask WHERE id = @TaskId";
+
+            var affected = Connection.Execute(removeQuery, param);
+            if (affected == 0) throw new Exception("Remove failed");
+            int removedId = param.TaskId;
+            return removedId;
+        }
+    }
+}
---
+++ ToDoAppUnitTests/Commands/RemoveTaskCommandUnitTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Npgsql;
+using Dapper;
+using ToDoAppDomainLayer.DataObjects;
+using ToDoAppDataAccessLayer.Commands;
+using ToDoAppDomainLayer.Parameters.Commands;
+
+namespace ToDoAppUnitTests.Commands
+{
+    [Collection("Sequential")]
+    public class RemoveTaskCommandUnitTests : TestBase
+    {
+        [Fact]
+        public void Test_RemovesOnValidInput()
+        {
+            // Arrange
+            int removedId = 1;
+            using var connection = new NpgsqlConnection(ConnectionString);
+            RemoveTaskCommand sut = new RemoveTaskCommand(connection);
+            // Act
+            sut.Execute(new RemoveTaskCommandParameter
+            {
+                TaskId = removedId,
+            });
+            // Assert
+            var count = connection.ExecuteScalar<int>("SELECT COUNT(*) FROM todotask");
+            Assert.Equal(5, count);
+            var removed = connection.Query<ToDoTask>("SELECT (id, title, description, createdat, " +
+                "isactive, iscomplete, categoryid) FROM todotask " +
+                "WHERE id = @Id",
+                new
+                {
+                    Id = removedId,
+                })
+                .FirstOrDefault();
+            Assert.Null(removed);
+        }
+
+        [Fact]
+        public void Test_ThrowsOnNonExistingId()
+        {
+            int removedId = 7; // invalidId
+            using var connection = new NpgsqlConnection(ConnectionString);
+            RemoveTaskCommand sut = new RemoveTaskCommand(connection);
+            // Act
+            // Assert
+            Assert.ThrowsAny<Exception>(() =>
+            {
+                sut.Execute(new RemoveTaskCommandParameter
+                {
+                    TaskId = removedId,
+                });
+            });
+        }
+    }
+}
