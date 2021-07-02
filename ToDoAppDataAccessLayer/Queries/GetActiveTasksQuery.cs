ed81b42b86dc340ab1f285e476a150634c949081
Alexey Gribov <m0chk0z@gmail.com>
2021-07-02 17:47:22 +0300

    Implement GetActiveTasks query, related parameter and unit tests

---
+++ ToDoAppDataAccessLayer/Queries/GetActiveTasksQuery.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using Dapper;
+using ToDoAppDomainLayer.Interfaces;
+using ToDoAppDomainLayer.Parameters.Queries;
+using ToDoAppDomainLayer.DataObjects;
+
+namespace ToDoAppDataAccessLayer.Queries
+{
+    public class GetActiveTasksQuery
+        : IQuery<GetActiveTasksQueryParameter, IEnumerable<ToDoTaskOutputModel>>
+    {
+        private NpgsqlConnection Connection { get; set; }
+
+        public GetActiveTasksQuery(NpgsqlConnection connection)
+        {
+            if (connection is null)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
+            this.Connection = connection;
+        }
+        public IEnumerable<ToDoTaskOutputModel> Execute(GetActiveTasksQueryParameter param)
+        {
+            string selectQuery = "SELECT todotask.id, todotask.title, todotask.description, todotask.createdat, " +
+                "todotask.isactive, todotask.iscomplete, todotask.categoryId, category.title AS categoryTitle, " +
+                "category.color AS categoryColor FROM todotask JOIN category ON todotask.categoryId = category.id " +
+                "WHERE isactive = true";
+            return Connection.Query<ToDoTaskOutputModel>(selectQuery, param).ToList();
+        }
+    }
+}
