df4b7c2651b2afd60429c5e661bb2bd29ee85ce2
Alexey Gribov <m0chk0z@gmail.com>
2021-07-02 17:46:40 +0300

    Implement GetCategories query, related parameter and unit tests

---
+++ ToDoAppDataAccessLayer/Queries/GetCategoriesQuery.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using Dapper;
+using ToDoAppDomainLayer.Interfaces;
+using ToDoAppDomainLayer.Parameters.Queries;
+using ToDoAppDomainLayer.DataObjects;
+
+namespace ToDoAppDataAccessLayer.Queries 
+{
+    public class GetCategoriesQuery : IQuery<GetCategoriesQueryParameter, IEnumerable<Category>>
+    {
+        private NpgsqlConnection Connection { get; set; }
+
+        public GetCategoriesQuery(NpgsqlConnection connection)
+        {
+            if (connection is null)
+            {
+                throw new ArgumentNullException(nameof(connection));
+            }
+            this.Connection = connection;
+        }
+
+        public IEnumerable<Category> Execute(GetCategoriesQueryParameter param)
+        {
+            string selectQuery = "SELECT (id, title, color) FROM category";
+            return Connection.Query<Category>(selectQuery).ToList();
+        }
+    }
+}
---
+++ ToDoAppUnitTests/Queries/GetCategoriesQueryUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Npgsql;
+using Dapper;
+using ToDoAppDomainLayer.DataObjects;
+using ToDoAppDataAccessLayer.Queries;
+using ToDoAppDomainLayer.Parameters.Queries;
+
+namespace ToDoAppUnitTests.Queries
+{
+    [Collection("Sequential")]
+    public class GetCategoriesQueryUnitTests : TestBase
+    {
+        [Fact]
+        public void Test_ReturnsCorrectCollectionSize()
+        {
+            // Arrange
+            using var connection = new NpgsqlConnection(ConnectionString);
+            GetCategoriesQuery sut = new GetCategoriesQuery(connection);
+            // Act
+            var result = sut.Execute(new GetCategoriesQueryParameter());
+            // Assert
+            Assert.Equal(4, result.Count());
+        }
+    }
+}
