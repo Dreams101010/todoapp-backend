20f1dfdb9576d14c88cfc90b80d30644f1a46320
Alexey Gribov <m0chk0z@gmail.com>
2021-07-02 17:42:28 +0300

    Implement data models for commands and queries

---
+++ ToDoAppDomainLayer/DataObjects/ToDoTaskOutputModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoAppDomainLayer.DataObjects
+{
+    public class ToDoTaskOutputModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsComplete { get; set; }
+        public int CategoryId { get; set; }
+        // embedded related category data
+        public string CategoryTitle { get; set; }
+        public string CategoryColor { get; set; }
+    }
+}
