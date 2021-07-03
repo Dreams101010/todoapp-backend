f7b3d932bacc1447339e4448ec1d43c0a16a52a7
Alexey Gribov <m0chk0z@gmail.com>
2021-07-03 13:23:52 +0300

    Implement custom application-level exception classes

---
+++ ToDoAppDomainLayer/Exceptions/DatabaseEntryNotFoundException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoAppDomainLayer.Exceptions
+{
+    public class DatabaseEntryNotFoundException : Exception
+    {
+        public DatabaseEntryNotFoundException(string message) : base(message)
+        {
+
+        }
+
+        public DatabaseEntryNotFoundException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+
+        }
+    }
+}
---
+++ ToDoAppDomainLayer/Exceptions/DatabaseInteractionException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoAppDomainLayer.Exceptions
+{
+    public class DatabaseInteractionException : Exception
+    {
+        public DatabaseInteractionException(string message) : base(message)
+        {
+
+        }
+
+        public DatabaseInteractionException(string message, Exception innerException) 
+            : base(message, innerException)
+        {
+
+        }
+    }
+}
